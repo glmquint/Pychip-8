71c95d15ff02206366f16b4ca98b3c0842c31504
Guillaume Quint <g.quint@studenti.unipi.it>
2022-07-30 13:25:55 +0200

    200% faster drawing and per-update display showing

--- chip.py
+++ chip.py
@@ -6,6 +6,8 @@ from struct import unpack, pack
 import traceback
 from time import time
 
+DEBUG = False
+
 def here(msg=''):
     print(f"REACHED HERE ({msg})")
     quit()
@@ -15,7 +17,18 @@ def usage(argv0):
 
 def debug_bitplane(bitplane, width):
     for i in range(0, len(bitplane), width):
-        print(bitplane[i:i+width])
+        log(bitplane[i:i+width])
+
+def show_disasm(disasm):
+    if DEBUG:
+        print(f"[DISM] {disasm};")
+
+def log(msg, end='\n'):
+    if DEBUG:
+        if end != '\n':
+            print(f"{msg}", end=end)
+        else:
+            print(f"[DEBUG]: {msg}")
 
 SCREEN_WIDTH = 64
 SCREEN_HEIGHT = 32
@@ -29,6 +42,7 @@ class Chip:
     delay_timer = 0x00      
     sound_timer = 0x00      
     V           = [0x00]*16 
+    redraw      = False
 
     def dump(self, show_memory=True, start=0, end=-1):
         if start == -1:
@@ -36,32 +50,32 @@ class Chip:
             end += start
         elif end < 0:
             end = len(self.memory)
-        print(f"\t\t===BEGIN DUMP (from {start} to {end})===")
+        log(f"\t\t===BEGIN DUMP (from {start} to {end})===")
         bytes_per_line = 8
         if show_memory:
             for y in range(start, end, bytes_per_line):
-                print(f"{y:#0{5}x}\t| ", end='')
+                log(f"{y:#0{5}x}\t| ", end='')
                 for x in range(bytes_per_line):
-                    print(f"{self.memory[y + x]:#0{4}x} ", end='')
-                print("|")
-        print("display = ")
+                    log(f"{self.memory[y + x]:#0{4}x} ", end='')
+                log("|", end=' \n')
+        log("display = ")
         debug_bitplane(self.display, SCREEN_WIDTH)
 
-        print(f"pc = {hex(self.pc)} ({self.pc})")
-        print(f"I = {hex(self.I)} ({self.I})")
-        print(f"sp = {hex(self.sp)} ({self.sp})")
-        print(f"delay_timer = {hex(self.delay_timer)} ({self.delay_timer})")
-        print(f"sound_timer = {hex(self.sound_timer)} ({self.sound_timer})")
+        log(f"pc = {hex(self.pc)} ({self.pc})")
+        log(f"I = {hex(self.I)} ({self.I})")
+        log(f"sp = {hex(self.sp)} ({self.sp})")
+        log(f"delay_timer = {hex(self.delay_timer)} ({self.delay_timer})")
+        log(f"sound_timer = {hex(self.sound_timer)} ({self.sound_timer})")
         for x in range(len(self.V)):
-            print(f"V{x} = {hex(self.V[x])} ({self.V[x]})")
-        print("\t\t===END DUMP===")
+            log(f"V{x} = {hex(self.V[x])} ({self.V[x]})")
+        log("\t\t===END DUMP===")
 
     def fetch(self):
         self.pc += 2
         return self.memory[self.pc-2:self.pc]
 
     def decode_and_execute(self, data):
-        print(f"decoding {data.hex() = }")
+        log(f"decoding {data.hex() = }")
         high_nibble = byte_and(data, b'\xF0\x00')
         if data == b'\x00\xE0':
             self.cls()
@@ -79,15 +93,17 @@ class Chip:
             raise Exception(f"Unimplemented instruction ({data.hex()} @ pc:{self.pc.hex()})")
 
     def render(self):
-        #print(f"now rendering {len(self.display)} pixels:")
+        #log(f"now rendering {len(self.display)} pixels:")
         #debug_bitplane(self.display, SCREEN_WIDTH)
         draw(SCREEN_WIDTH, SCREEN_HEIGHT, self.display)
+        self.redraw = False
 
 
 
     def cls(self):
         self.display = '0'*(SCREEN_WIDTH * SCREEN_HEIGHT)
-        print(f"[DISM] cls;")
+        self.redraw = True
+        show_disasm("cls")
 
     def jp_addr(self, data):
         jmp_addr = unpack('>H', byte_and(data, b'\x0f\xff'))[0]
@@ -95,40 +111,44 @@ class Chip:
             raise Exception("Halt")
         else:
             self.pc = jmp_addr
-        print(f"[DISM] jp {jmp_addr};")
+        show_disasm(f"jp {jmp_addr}")
 
     def ld_vx_byte(self, data):
         x = unpack('<B', byte_and(bytes([data[0]]), b'\x0f'))[0]
         self.V[x] = data[1]
-        print(f"[DISM] ld v{x} {hex(data[1])};")
+        show_disasm(f"ld v{x} {hex(data[1])}")
 
     def add_vx_byte(self, data):
         x = unpack('<B', byte_and(bytes([data[0]]), b'\x0f'))[0]
         self.V[x] += data[1]
-        print(f"[DISM] add v{x} {data[1]};")
+        show_disasm(f"add v{x} {data[1]}")
 
     def ld_i_addr(self, data):
         a = byte_and(data, b'\x0f\xff')
         self.I = unpack('>H', a)[0]
-        print(f"[DISM] ld I {a.hex()};")
+        show_disasm(f"ld I {a.hex()}")
 
     def drw_vx_vy_nibble(self, data):
         x = unpack('>B', byte_and(bytes([data[0]]), b'\x0f'))[0]
         y = unpack('>B', byte_and(bytes([data[1]]), b'\xf0'))[0] >> 4
         n = unpack('>B', byte_and(bytes([data[1]]), b'\x0f'))[0]
-        sprite = self.memory[self.I:self.I+n]
-        #print(f"{x = } V[{x}] = {hex(self.V[x])}, {y = } V[{y}] = {hex(self.V[y])}, {n = }, {hex(self.I) = }, {sprite = }")
-        
         for i in range(n):
-            bit_sprite = f"{sprite[i]:08b}"
-            offset = (self.V[y]+i)*SCREEN_WIDTH + self.V[x]
-            new_plane = '0'*(offset) + bit_sprite + '0'*(SCREEN_WIDTH*SCREEN_HEIGHT - len(bit_sprite) - offset)
-            res_plane = ''
-            for a, b in zip(self.display, new_plane):
-                res_plane += str(int(a) | int(b))
-            self.display = res_plane
+            if self.V[y] + i > SCREEN_HEIGHT:
+                break
+            bit_sprite_row = f"{self.memory[self.I + i]:08b}"
+            for j, bit in enumerate(bit_sprite_row):
+                offset = (self.V[y]+i)*SCREEN_WIDTH + self.V[x] + j
+                if self.V[x] + j > SCREEN_WIDTH:
+                    break
+                if bit == '1':
+                    if self.display[offset] == '1':
+                        self.display = self.display[:offset] + '0' + self.display[offset+1:]
+                        self.V[0xF] = 1
+                    else:
+                        self.display = self.display[:offset] + '1' + self.display[offset+1:]
+        self.redraw = True
 
-        print(f"[DISM] drw v{x} v{y} {n};")
+        show_disasm(f"drw v{x} v{y} {n}")
 
 
 
@@ -152,8 +172,10 @@ if __name__ == '__main__':
         except Exception as e:
             here(traceback.format_exc())
         #chip.dump(True, -1, 60)
-        chip.render()
+        if chip.redraw:
+            chip.render()
         elapsed = time()-start_time
-        print(f"time elapsed: {elapsed}, FPS = {1/elapsed}")
+        print(f"time elapsed: {elapsed}, FPS = {1/elapsed}\r", end='')
+        #input()
         wait_time = max(0, 1/60 - elapsed)
         sleep(wait_time)
